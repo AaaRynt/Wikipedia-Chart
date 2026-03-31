df8cfc2a30cfdc928d45cc7fa5f0bf1c38482a5f
RynT <122379612+AaaRynt@users.noreply.github.com>
2026-03-31 19:44:12 +0800

    Shadcn/ui Recharts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-// src/App.tsx
+// src/components/App.tsx
 export default function App() {
   return <div className="flex min-h-screen flex-col items-center bg-[#282c34] text-gray-200">Hello, World!</div>
 }
